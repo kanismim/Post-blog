51a9d6c9221501b557d9f18063e5c5df4596a9de
kanis mim <kanismim075@gmail.com>
2020-12-22 01:28:53 +0600

    Posts view

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using PostBlog.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PostBlog.Controllers
+{
+    [RoutePrefix("api/post")]
+    public class PostsController : ApiController
+    {
+        PostRepository postrepo = new PostRepository();
+
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(postrepo.GetAll());
+        }
+       
+    }
+}
---
+++ Repositories/PostRepository.cs
@@ -0,0 +1,12 @@
+﻿using PostBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostBlog.Repositories
+{
+    public class PostRepository: Repository<Post>
+    {
+    }
+}
